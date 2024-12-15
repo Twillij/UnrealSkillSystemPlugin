e0bfdbee8a9e30b6f499994b6d305f92987656a0
Will <silentwill29@gmail.com>
2024-12-15 17:22:58 +1100

    Moved input binding from component to function lib

---
+++ Source/SkillSystem/SkillFunctionLibrary.cpp
@@ -0,0 +1,27 @@
+﻿#include "SkillFunctionLibrary.h"
+#include "Skill.h"
+#include "EnhancedInputComponent.h"
+
+bool USkillFunctionLibrary::BindSkillToInput(const APlayerController* PlayerController, const FName InputActionName,
+	const EInputEvent InputEvent, USkill* Skill)
+{
+	if (!Skill || !PlayerController || !PlayerController->InputComponent || !PlayerController->IsLocalPlayerController())
+		return false;
+
+	FInputActionBinding& Binding = PlayerController->InputComponent->BindAction(InputActionName, InputEvent, Skill, &USkill::ActivateSkill);
+	return true;
+}
+
+bool USkillFunctionLibrary::BindSkillToEnhancedInput(const APlayerController* PlayerController, const UInputAction* InputAction,
+	const ETriggerEvent TriggerEvent, USkill* Skill)
+{
+	if (!InputAction || !Skill || !PlayerController || !PlayerController->InputComponent || !PlayerController->IsLocalPlayerController())
+		return false;
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+	{
+		FEnhancedInputActionEventBinding& Binding = EnhancedInputComponent->BindAction(InputAction, TriggerEvent, Skill, &USkill::ActivateSkill);
+		return true;
+	}
+	return false;
+}
---
+++ Source/SkillSystem/SkillFunctionLibrary.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SkillFunctionLibrary.generated.h"
+
+enum class ETriggerEvent : uint8;
+class UInputAction;
+class USkill;
+
+UCLASS()
+class SKILLSYSTEM_API USkillFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	static bool BindSkillToInput(const APlayerController* PlayerController, const FName InputActionName,
+		const EInputEvent InputEvent, USkill* Skill);
+
+	static bool BindSkillToEnhancedInput(const APlayerController* PlayerController, const UInputAction* InputAction,
+		const ETriggerEvent TriggerEvent, USkill* Skill);
+};
